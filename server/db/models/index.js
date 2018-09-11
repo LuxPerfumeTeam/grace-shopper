085baa64fd0cba3b97a1378771de34676a82fb8b
Tricia Lobo <tricia@Tricias-MacBook-Pro.local>
2018-09-11 12:22:31 -0400

    associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,6 +4,19 @@ const Products = require('./products')
 const Categories = require('./categories')
 const Orders = require('./orders')
 
+//reviews belongs to product
+//product has many reviews
+//user needs order id
+Products.belongsTo(Reviews, {as: 'productReviews'})
+User.belongsTo(Reviews, {as: 'userReviews'})
+
+User.belongsTo(Orders, {as: 'userOrders'})
+Orders.belongsToMany(Products, {through: 'orderProducts'})
+Products.belongsToMany(Orders, {through: 'orderProducts'})
+// Reviews.belongsTo(User)
+// Reviews.belongsTo(Products)
+// Products.hasMany(Reviews)
+
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
