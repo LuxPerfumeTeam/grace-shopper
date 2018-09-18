410d0a33f66e19fa3aa913284d2d63d934a0650f
Valerie Rojdestvenskaia <vrojdestvenskaia@gmail.com>
2018-09-18 15:39:39 -0400

    yas

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react'
+import React, {Fragment, Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link, withRouter} from 'react-router-dom'
@@ -6,76 +6,111 @@ import {logout} from '../store'
 
 const categoryArr = ['Men', 'Women']
 
-const Navbar = props => {
-  const {handleClick, isLoggedIn, isLoggedInAdmin} = props
+class Navbar extends Component {
+  constructor() {
+    super()
+    this.state = {
+      search: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-  return (
-    <div>
-      <nav>
-        <div className="nav-wrapper black-text">
-          <Link
-            to="/"
-            className="brand-logo center black-text v-align wrapper"
-            onClick={() => location.reload()}
-          >
-            LUXPERFUME
-          </Link>
-          <ul className="left hide-on-med-and-down black-text">
-            {categoryArr.map(each => {
-              return (
-                <li key={each}>
-                  <Link to={`/category/${each}`}>{each}</Link>
-                </li>
-              )
-            })}
-          </ul>
-          <ul className="right hide-on-med-and-down">
-            <li>
-              <Link to="/cart">
-                <i className="material-icons">add_shopping_cart</i>
-              </Link>
-            </li>
-            {isLoggedIn && !isLoggedInAdmin ? (
-              <Fragment>
-                <li>
-                  <Link to="/home">My Account</Link>
-                </li>
-                <li>
-                  <a href="#" onClick={handleClick}>
-                    Logout
-                  </a>
-                </li>
-              </Fragment>
-            ) : isLoggedInAdmin ? (
-              <Fragment>
-                <li>
-                  <Link to="/admin/home">My Admin Account</Link>
-                  <Link to="/admin/addproduct">Add/Edit Products</Link>
-                  <Link to="/admin/addcategory">Add/Edit Categories</Link>
-                  <Link to="/admin/orders">Orders</Link>
-                </li>
-                <li>
-                  <a href="#" onClick={handleClick}>
-                    Logout
-                  </a>
-                </li>
-              </Fragment>
-            ) : (
-              <Fragment>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link to="/signup">Sign Up</Link>
-                </li>
-              </Fragment>
-            )}
-          </ul>
-        </div>
-      </nav>
-      <hr />
-    </div>
-  )
+  handleChange(evt) {
+    this.setState({[evt.target.name]: evt.target.value})
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault()
+
+    const product = this.state
+
+    const id = this.props.id
+    this.props.editProduct(id, product)
+    this.props.history.push(`/`)
+  }
+
+  render() {
+    const {handleClick, isLoggedIn, isLoggedInAdmin} = this.props
+
+    return (
+      <div>
+        <nav>
+          <div className="nav-wrapper black-text">
+            <Link
+              to="/"
+              className="brand-logo center black-text v-align wrapper"
+              onClick={() => location.reload()}
+            >
+              LUXPERFUME
+            </Link>
+            <div className="left hide-on-med-and-down black-text">
+              <input
+                name="search"
+                type="text"
+                value={this.state.search}
+                placeholder="search"
+                onChange={this.handleChange}
+                onSubmit={this.handleSubmit}
+              />
+            </div>
+            <ul className="left hide-on-med-and-down black-text">
+              {categoryArr.map(each => {
+                return (
+                  <li key={each}>
+                    <Link to={`/category/${each}`}>{each}</Link>
+                  </li>
+                )
+              })}
+            </ul>
+            <ul className="right hide-on-med-and-down">
+              <li>
+                <Link to="/cart">
+                  <i className="material-icons">add_shopping_cart</i>
+                </Link>
+              </li>
+              {isLoggedIn && !isLoggedInAdmin ? (
+                <Fragment>
+                  <li>
+                    <Link to="/home">My Account</Link>
+                  </li>
+                  <li>
+                    <a href="#" onClick={handleClick}>
+                      Logout
+                    </a>
+                  </li>
+                </Fragment>
+              ) : isLoggedInAdmin ? (
+                <Fragment>
+                  <li>
+                    <Link to="/admin/home">My Admin Account</Link>
+                    <Link to="/admin/addproduct">Add/Edit Products</Link>
+                    <Link to="/admin/addcategory">Add/Edit Categories</Link>
+                    <Link to="/admin/orders">Orders</Link>
+                  </li>
+                  <li>
+                    <a href="#" onClick={handleClick}>
+                      Logout
+                    </a>
+                  </li>
+                </Fragment>
+              ) : (
+                <Fragment>
+                  <li>
+                    <Link to="/login">Login</Link>
+                  </li>
+                  <li>
+                    <Link to="/signup">Sign Up</Link>
+                  </li>
+                </Fragment>
+              )}
+            </ul>
+          </div>
+        </nav>
+        <hr />
+      </div>
+    )
+  }
 }
 
 /**
